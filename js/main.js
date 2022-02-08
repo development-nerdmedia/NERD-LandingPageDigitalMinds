5d0c427a6340cbc587fc1ceab4909d84dff837ef
Diego Castillo <88866352+diegocastillosaldana@users.noreply.github.com>
2022-02-08 17:47:48 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,4 +3,62 @@ document.addEventListener("click", function (e) {
         document.querySelector(".menupage").classList.toggle("open");
         // jQuery('body').addClass('scrollhidden');
     }
+    if (e.target.closest(".brandingbtn")) {
+        document.querySelector(".branding").classList.toggle("open");
+        // jQuery('body').addClass('scrollhidden');
+    }
+    if (e.target.closest(".marketingbtn")) {
+        document.querySelector(".marketing").classList.toggle("open");
+        // jQuery('body').addClass('scrollhidden');
+    }
+    if (e.target.closest(".disenowebbtn")) {
+        document.querySelector(".disenoWeb").classList.toggle("open");
+        // jQuery('body').addClass('scrollhidden');
+    }
+
+    /* cerrar */
+    if (e.target.closest(".cerrarr")) {
+        document.querySelector(".menupage").classList.toggle("open");
+        // jQuery('body').removeClass('scrollhidden');
+    }
+    if (e.target.closest(".closebranding")) {
+        document.querySelector(".branding").classList.toggle("open");
+        // jQuery('body').removeClass('scrollhidden');
+    }
+    if (e.target.closest(".closemarketing")) {
+        document.querySelector(".marketing").classList.toggle("open");
+        // jQuery('body').removeClass('scrollhidden');
+    }
+    if (e.target.closest(".closedisenoweb")) {
+        document.querySelector(".disenoWeb").classList.toggle("open");
+        // jQuery('body').removeClass('scrollhidden');
+    }
+})
+
+$('.clients-carousel').owlCarousel({
+    autoplay: true,
+    loop: true,
+    margin: 15,
+    dots: false,
+    slideTransition: 'linear',
+    autoplayTimeout: 4500,
+    autoplayHoverPause: true,
+    autoplaySpeed: 4500,
+    responsive: {
+        0: {
+            items: 2
+        },
+        500: {
+            items: 3
+        },
+        600: {
+            items: 4
+        },
+        800: {
+            items: 4
+        },
+        1200: {
+            items: 6
+        },
+    }
 })
