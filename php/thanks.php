25420d940b7bceca4f6a4312a823b5761c3ddf28
Diego Castillo <88866352+diegocastillosaldana@users.noreply.github.com>
2022-03-08 15:52:07 -0500

    Add files via upload

--- php/thanks.php
+++ php/thanks.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="icon" type="image/png" href="../img/favicon-1.png" />
+    <link rel="stylesheet" href="../css/style.css" />
     <title>NERD - Landing Page Digital Minds</title>
   </head>
   <body>
@@ -18,7 +19,7 @@ $to = $myemails;
 $email_subject = "Contacto Nerd Digital";
 $email_body = "Haz recibido un nuevo mensaje. \n Nombre: $name \n 
 Telefono: $tel \n Mensaje: \n $message";
-$headers = "From: $email";
+$headers = "From: digitalminds.site";
 
 mail($to, $email_subject, $email_body, $headers);
 ?>
@@ -27,13 +28,13 @@ mail($to, $email_subject, $email_body, $headers);
         <div class="content">
           <div class="top">
             <a class="closeinterna" href="../index.html">
-              <img src="img/closemenu.svg" alt="close"
+              <img src="../img/closemenu.svg" alt="close"
             /></a>
           </div>
           <div class="text">
             <h2>¡GRACIAS!</h2>
             <p>En breve nos contactaremos</p>
-            <img src="img/img-thanks.png" alt="" />
+            <img src="../img/img-thanks.png" alt="" />
           </div>
         </div>
       </div>
